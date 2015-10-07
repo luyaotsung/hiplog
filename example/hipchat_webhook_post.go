2ed76b6767f2bef7403c656d3b2bdf37f7db0d5a
Eli Lu <eli_lu@htc.com>
2015-10-07 15:35:04 +0800

    Add example

---
+++ example/hipchat_webhook_post.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/tbruyelle/hipchat-go/hipchat"
+	"log"
+	"net/http"
+	"os"
+)
+
+type HipChatEvent struct {
+	Event string           `json:"event"`
+	Item  HipChatEventItem `json:"item"`
+}
+
+type HipChatEventItem struct {
+	Message HipChatEventMessage `json:"message"`
+	Room    HipChatRoom         `json:"room"`
+}
+
+type HipChatEventMessage struct {
+	Date          string
+	File          HipChatFile `json:"file,omitempty"`
+	From          interface{}
+	Message       string
+	Color         string
+	Type          string `json:"type,omitempty"`
+	Id            string
+	MessageFormat string `json:"message_format"`
+}
+
+type HipChatUser struct {
+	Id      int    `json:"id"`
+	Mention string `json:"mention_name"`
+	Name    string `json:"name"`
+}
+
+type HipChatFile struct {
+	Name     string `json:"name"`
+	Size     int    `json:"size"`
+	ThumbUrl string `json:"thumb_url"`
+	Url      string `json:"url"`
+}
+
+type HipChatRoom struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+var (
+	AccessToken = "tNgowr8imQKkISK3LBI1cHDVXmkjxUPcvlmktwen"
+	RoomID      = "730783"
+)
+
+func writeToFile(f *os.File, sourceRoom HipChatRoom, sourceMessage HipChatEventMessage) error {
+
+	strFrom, ok := sourceMessage.From.(string)
+
+	if !ok {
+		str, err := json.Marshal(sourceMessage.From)
+		if err != nil {
+			return err
+		}
+		var user HipChatUser
+		err = json.Unmarshal([]byte(str), &user)
+		if err != nil {
+			return err
+		}
+		strFrom = user.Name
+	}
+
+	fmt.Sprintf("[%s|%s] %s: %s\n", sourceMessage.Date, sourceRoom.Name, strFrom, sourceMessage.Message)
+
+	Msg := fmt.Sprintf("Your Input Message is %s", sourceMessage.Message)
+
+	send_Notify(AccessToken, RoomID, Msg, "red")
+
+	msg := fmt.Sprintf("[%s|%s] %s: %s\n", sourceMessage.Date, sourceRoom.Name, strFrom, sourceMessage.Message)
+	_, err := f.WriteString(msg)
+	return err
+}
+
+func handler(w http.ResponseWriter, r *http.Request, outFile *os.File) {
+	var notifyEvent HipChatEvent
+
+	json.NewDecoder(r.Body).Decode(&notifyEvent)
+
+	err := writeToFile(outFile, notifyEvent.Item.Room, notifyEvent.Item.Message)
+	if err != nil {
+		w.WriteHeader(200)
+	} else {
+		w.WriteHeader(500)
+	}
+}
+
+func send_Notify(token string, id string, message string, color string) {
+
+	c := hipchat.NewClient(token)
+
+	notifRq := &hipchat.NotificationRequest{Message: message, Color: color}
+	resp2, err := c.Room.Notification(id, notifRq)
+
+	if err != nil {
+		fmt.Printf("Error during room notification %q\n", err)
+		fmt.Printf("Server returns %+v\n", resp2)
+	}
+}
+
+func main() {
+
+	filePath := "hip.log"
+	out, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("can not open the log file: %s, err: %v", filePath, err)
+	}
+	defer out.Close()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		handler(w, r, out)
+	})
+	http.ListenAndServe(":8088", nil)
+}
